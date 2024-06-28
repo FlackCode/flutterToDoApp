39724c19c53c0c4be1b56bf053f26567a775129b
flack <163582560+FlackCode@users.noreply.github.com>
2024-06-28 09:07:46 +0300

    Task Display and Check to complete task

---
+++ lib/util/todo_tile.dart
@@ -0,0 +1,43 @@
+import "package:flutter/material.dart";
+
+// ignore: must_be_immutable
+class ToDoTile extends StatelessWidget {
+  final String taskName;
+  final bool taskCompleted;
+  Function(bool?)? onChanged;
+
+  ToDoTile(
+      {super.key,
+      required this.taskName,
+      required this.taskCompleted,
+      required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25, right: 25, top: 25),
+      child: Container(
+        padding: const EdgeInsets.all(24),
+        decoration: BoxDecoration(
+            color: Colors.blue, borderRadius: BorderRadius.circular(12)),
+        child: Row(
+          children: [
+            Checkbox(
+              value: taskCompleted,
+              onChanged: onChanged,
+              fillColor: WidgetStateProperty.all(
+                  taskCompleted ? Colors.blue[200] : Colors.blue),
+            ),
+            Text(
+              taskName,
+              style: TextStyle(
+                  decoration: taskCompleted
+                      ? TextDecoration.lineThrough
+                      : TextDecoration.none),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
